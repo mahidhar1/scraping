43c8283672a273f3201a58627cd4215108b76bb3
mahidhar1 <mnyayapati@gmail.com>
2020-02-25 16:51:50 +0530

    modify the finviz scrapper code to include dataframes and functions

--- FINVIZ_NEWS_SCRAPER/finviz_news_scrapper.py
+++ FINVIZ_NEWS_SCRAPER/finviz_news_scrapper.py
@@ -7,29 +7,42 @@ Created on Sat Feb 22 22:31:51 2020
 
 import bs4 as bs
 import requests
-
-url = r"https://www.finviz.com/news.ashx"
-r = requests.get(url)
-soup = bs.BeautifulSoup(r.content)
+import pandas as pd
 
 
-raw_data = soup.findAll("div", {"class": "news"})
 
-
-tables = raw_data[0].find_all("table")
-
-news = tables[3].find_all("tr")
-blogs = tables[4].find_all("tr")
-
-data_news = []
-data_blogs = []
-
-for row in news:
-    td = row.find_all("td")
-    d = [el.text for el in td] + [td[-1].find_all('a')[0]["href"]]
-    data_news.append(d)
+def get_news(tables):
+    news = tables[3].find_all("tr")
+    data_news = []
+    for row in news:
+        td = row.find_all("td")
+        d = [el.text for el in td] + [td[-1].find_all('a')[0]["href"]]
+        data_news.append(d)
     
-for row in blogs:
-    td = row.find_all("td")
-    d = [el.text for el in td] + [td[-1].find_all('a')[0]["href"]]
-    data_blogs.append(d)
+    return pd.DataFrame(data_news)
+
+
+def get_blogs(tables):
+    blogs = tables[4].find_all("tr")
+    data_blogs = []    
+    for row in blogs:
+        td = row.find_all("td")
+        d = [el.text for el in td] + [td[-1].find_all('a')[0]["href"]]
+        data_blogs.append(d)
+    return pd.DataFrame(data_blogs)
+
+
+if __name__ == "__main__":
+    url = r"https://www.finviz.com/news.ashx"
+    r = requests.get(url)
+    soup = bs.BeautifulSoup(r.content)
+
+    raw_data = soup.findAll("div", {"class": "news"})
+    tables = raw_data[0].find_all("table")
+    
+    data_news = get_news(tables)
+    data_blogs = get_blogs(tables)
+    
+        
+    
+    
